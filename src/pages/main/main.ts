05c72ad819f1c2fcc66d20b554cd32d07a3a465b
Mohd Mazlan Bin Mohd Nor <lan.psis@gmail.com>
2018-08-31 09:48:34 +0800

    minor changes
    Signed-off-by: mazlanmohdnor <lan.psis@gmail.com>

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import { Clipboard } from '@ionic-native/clipboard';
-import { DeviceFeedback } from '@ionic-native/device-feedback';
-import { Storage } from "@ionic/storage";
-import { AlertController, IonicPage, LoadingController, ModalController, NavController, ToastController, Events } from 'ionic-angular';
-import { DataProvider } from './../../providers/data/data';
-import { PosApiProvider } from './../../providers/pos-api/pos-api';
+import {Component} from '@angular/core';
+import {Clipboard} from '@ionic-native/clipboard';
+import {DeviceFeedback} from '@ionic-native/device-feedback';
+import {Storage} from "@ionic/storage";
+import {
+  AlertController,
+  Events,
+  IonicPage,
+  LoadingController,
+  ModalController,
+  NavController,
+  ToastController
+} from 'ionic-angular';
+import {DataProvider} from '../../providers/data/data';
+import {PosApiProvider} from '../../providers/pos-api/pos-api';
 import 'rxjs/add/operator/timeout';
 
 @IonicPage()
@@ -13,14 +21,14 @@ import 'rxjs/add/operator/timeout';
   templateUrl: 'main.html',
 })
 export class MainPage {
-  //nak check ada data ke xde, kalau xde, show div xde data
-  private hasData: boolean = false;
-  private loader;
   flag: any = false;
   iconList: string = 'list';
   myInput: string;
-  private displayItem: any[] = [];
   public storedata: { title: string, trackingNum: string, icon: string };
+  //nak check ada data ke xde, kalau xde, show div xde data
+  private hasData: boolean = false;
+  private loader;
+  private displayItem: any[] = [];
   private posData: any[] = [];
   //declare dataObject
   private dataObj: { title: string; trackingNum: string; data: any[]; };
@@ -53,7 +61,9 @@ export class MainPage {
               }
             });
 
-            if (this.displayItem.map(element => { return element.trackingNum }).indexOf(trackNo) == -1) {
+            if (this.displayItem.map(element => {
+              return element.trackingNum
+            }).indexOf(trackNo) == -1) {
               this.displayItem.push(this.storedata);
             }
           } else {
@@ -75,7 +85,7 @@ export class MainPage {
               title: element.title,
               trackingNum: element.trackingNum,
               icon: element.icon
-            }
+            };
             this.displayItem.push(this.storedata);
           });
         } else {
@@ -91,15 +101,15 @@ export class MainPage {
     setTimeout(() => {
       this.storage.get('tracking').then(x => {
         console.log('new', x)
-      })
+      });
       console.log('Async operation has ended');
       refresher.complete();
     }, 1500);
   }
 
   actionBtn() {
-    this.haptic.acoustic()
-    
+    this.haptic.acoustic();
+
     this.flag = !this.flag;
     if (this.flag) {
       this.iconList = 'checkmark-circle';
@@ -114,52 +124,52 @@ export class MainPage {
 
   filterItems(ev: any) {
     let val = ev.target.value;
-    console.log('val',this.myInput)
-    
-    // if (this.displayItem != undefined && this.displayItem.length != 0) {
-      if (!this.myInput) {  
-        console.log('no val', this.myInput)
-        this.dataProvider.getData.subscribe((result) => {
-          if (result != undefined && result != null) {
-             this.displayItem = result;
+    console.log('val', this.myInput);
 
-            console.log('this.displayItem',this.displayItem)
-             return this.displayItem;
-            // result.forEach(element => {
-            //   this.storedata = {
-            //     title: element.title,
-            //     trackingNum: element.trackingNum,
-            //     icon: element.icon
-            //   }
-            //   this.displayItem = [];
-            //   // if (this.displayItem.map(item => { return item.trackingNum }).indexOf(element.trackingNum) == -1) {
-            //     return this.displayItem.push(this.storedata);
-            //   // }
-            // });
-          }
-        })
-        // this.storage.get('tracking').then(x => {
-        //   console.log('new', x)
-        //   this.displayItem = x;
-        // })
-      } else {
-        console.log('val',this.myInput)
-        this.displayItem = this.displayItem.filter(item => {
-          return item.title.toLowerCase().includes(val.toLowerCase()) || item.trackingNum.toLowerCase().includes(val.toLowerCase())
-        });
-      }
+    // if (this.displayItem != undefined && this.displayItem.length != 0) {
+    if (!this.myInput) {
+      console.log('no val', this.myInput);
+      this.dataProvider.getData.subscribe((result) => {
+        if (result != undefined && result != null) {
+          this.displayItem = result;
+
+          console.log('this.displayItem', this.displayItem);
+          return this.displayItem;
+          // result.forEach(element => {
+          //   this.storedata = {
+          //     title: element.title,
+          //     trackingNum: element.trackingNum,
+          //     icon: element.icon
+          //   }
+          //   this.displayItem = [];
+          //   // if (this.displayItem.map(item => { return item.trackingNum }).indexOf(element.trackingNum) == -1) {
+          //     return this.displayItem.push(this.storedata);
+          //   // }
+          // });
+        }
+      })
+      // this.storage.get('tracking').then(x => {
+      //   console.log('new', x)
+      //   this.displayItem = x;
+      // })
+    } else {
+      console.log('val', this.myInput);
+      this.displayItem = this.displayItem.filter(item => {
+        return item.title.toLowerCase().includes(val.toLowerCase()) || item.trackingNum.toLowerCase().includes(val.toLowerCase())
+      });
+    }
     // }
   }
 
   reorderItems(indexes) {
 
-    console.log('before', this.displayItem)
+    console.log('before', this.displayItem);
     let element = this.displayItem[indexes.from];
 
-    console.log('element', element)
+    console.log('element', element);
 
     this.displayItem.splice(indexes.from, 1);
-    console.log('deleted:', this.displayItem)
+    console.log('deleted:', this.displayItem);
 
     this.displayItem.splice(indexes.to, 0, element);
     console.log('after', this.displayItem)
@@ -175,7 +185,7 @@ export class MainPage {
         {
           text: 'No',
           handler: () => {
-            this.haptic.acoustic()
+            this.haptic.acoustic();
 
             console.log('Disagree clicked');
           }
@@ -183,16 +193,17 @@ export class MainPage {
         {
           text: 'Yes',
           handler: () => {
-            this.haptic.acoustic()
+            this.haptic.acoustic();
 
             console.log('Agree clicked');
             this.dataProvider.delete(index);
             this.navCtrl.setRoot(this.navCtrl.getActive().component);
           }
         }]
-    })
+    });
     confirm.present();
   }
+
   //show delete confirmation end
 
   //code 204
@@ -205,7 +216,7 @@ export class MainPage {
         {
           text: 'Ok',
           handler: data => {
-            this.haptic.acoustic()
+            this.haptic.acoustic();
 
             this.dismissLoading();
             // prompt.dismiss();
@@ -215,6 +226,7 @@ export class MainPage {
     });
     prompt.present();
   }
+
   //error 202 end
   //code 504
   showPrompt2() {
@@ -226,7 +238,7 @@ export class MainPage {
         {
           text: 'Ok',
           handler: data => {
-            this.haptic.acoustic()
+            this.haptic.acoustic();
 
             this.dismissLoading();
             prompt.dismiss();
@@ -236,6 +248,7 @@ export class MainPage {
     });
     prompt.present();
   }
+
   //error 504
   //code connectionTimeout
   connectionTimeout() {
@@ -247,7 +260,7 @@ export class MainPage {
         {
           text: 'Ok',
           handler: data => {
-            this.haptic.acoustic()
+            this.haptic.acoustic();
 
             this.dismissLoading();
             prompt.dismiss();
@@ -257,10 +270,11 @@ export class MainPage {
     });
     prompt.present();
   }
+
   //error connectionTimeout
 
   addTracking() {
-    this.haptic.acoustic()
+    this.haptic.acoustic();
     this.navCtrl.push('HomePage');
   }
 
@@ -280,48 +294,51 @@ export class MainPage {
       this.loader = null;
     }
   }
+
   // loader end
 
   //value kat sini datang dari html sana, pass value untuk setiap row
   //array of semua data
 
   trackDetail(value) {
-    this.haptic.acoustic()
+    this.haptic.acoustic();
     this.showLoading();
     this.pos.getDetail(value.trackingNum)
       .timeout(10000)
       .subscribe(result => {
-        // nak amik data je
-        for (var key in result.data) {
-          if (result.data.hasOwnProperty(key)) {
-            this.posData.push(result.data[key]);
+          // nak amik data je
+          for (var key in result.data) {
+            if (result.data.hasOwnProperty(key)) {
+              this.posData.push(result.data[key]);
+            }
           }
-        }
-        //store semua dalm object
-        this.dataObj = {
-          title: value.title,
-          trackingNum: value.trackingNum,
-          data: this.posData,
-        }
-        //nak check code ,204 error, 200 ok, 504 "Server SPR terlalu perlahan."
-        if (result.code == 200) {
+          //store semua dalm object
+          this.dataObj = {
+            title: value.title,
+            trackingNum: value.trackingNum,
+            data: this.posData,
+          };
+          //nak check code ,204 error, 200 ok, 504 "Server SPR terlalu perlahan."
+          if (result.code == 200) {
+            this.dismissLoading();
+            //send data to TrackprogressPage with dataObj
+            this.navCtrl.push('TrackprogressPage', {'dataObj': this.dataObj});
+          }
+          if (result.code == 204) {
+            this.showPrompt();
+          }
+          if (result.code == 504) {
+            this.showPrompt2();
+          }
+        },
+        error => {
           this.dismissLoading();
-          //send data to TrackprogressPage with dataObj
-          this.navCtrl.push('TrackprogressPage', { 'dataObj': this.dataObj });
-        } if (result.code == 204) {
-          this.showPrompt();
-        } if (result.code == 504) {
-          this.showPrompt2();
-        }
-      },
-      error => {
-        this.dismissLoading();
-        this.connectionTimeout();
-      });
+          this.connectionTimeout();
+        });
   }
 
   copy(i, trackingNum) {
-    this.haptic.acoustic()
+    this.haptic.acoustic();
     this.clipboard.copy(trackingNum).then(() => {
       let toast = this.toast.create({
         message: 'Copied to clipboard',
@@ -335,14 +352,14 @@ export class MainPage {
   }
 
   delete(index, title) {
-    this.haptic.acoustic()
+    this.haptic.acoustic();
     // this.value = ;
     // console.log(index);
     this.showConfirm(index, title);
   }
 
   about() {
-    this.haptic.acoustic()
+    this.haptic.acoustic();
     let modal = this.modalCtrl.create('InfoPage');
     modal.present();
   }
